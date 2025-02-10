b1fa73a4627d26e338745945b53fbd06da7e6ac2
Sean Kugele <kugeles@rhodes.edu>
2025-02-10 09:59:54 -0600

    Add display of number of cmd line args

--- DiS/2.9.2/args.c
+++ DiS/2.9.2/args.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+  printf("There are %d command line arguments!\n", argc);
   for (int i = 0; i < argc; i++) {
     printf("argv[%d]: %s\n", i, argv[i]);
   }
